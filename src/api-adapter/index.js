0ef44e38af9dff913585f6d6762a6d0da44dcac9
victor <vvazquez307@gmail.com>
2023-02-20 12:01:08 -0600

    debuggin failed

--- src/api-adapter/index.js
+++ src/api-adapter/index.js
@@ -1,14 +1,23 @@
 const BASE_URL = "https://strangers-things.herokuapp.com/api/2301-ftb-et-web-ft/posts"
 
 export const getFakePosts =  async () => {
+    // try{
+    //     const response = await fetch(BASE_URL)
+    //     const result = await response.json()
+    //     posts = result.posts
+    //     console.log(posts)
+	// 	return posts
+	// } catch (error) {
+	// 	console.log(error)
+	// }
+    
     try{
         const response = await fetch(BASE_URL, {
             method: "GET",
         });
         const result = await response.json();
-        console.log(result)
 		return result;
 	} catch (error) {
 		console.log(error);
 	}
-};
+}
